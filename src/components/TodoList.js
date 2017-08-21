8adbc62d45f728073ee20d78bb25388150cc1795
Victor Miguez <victorcineis@gmail.com>
2017-08-20 21:41:21 -0300

    Fetch todos with redux-thunk

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchTodos } from '../reducers/todo';
 
 const TodoItem = ({ name, isComplete }) => (
   <li>
@@ -11,18 +12,28 @@ const TodoItem = ({ name, isComplete }) => (
   </li>
 )
 
-const TodoList = (props) => (
-  <div className="Todo-List">
-    <ul>
-      {props.todos.map((todo => (
-        <TodoItem key={todo.id} {...todo} />
-      )))}
-    </ul>
-  </div>
-);
+class TodoList extends Component {
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+
+  render() {
+    return (
+      <div className="Todo-List">
+        <ul>
+          {this.props.todos.map((todo => (
+            <TodoItem key={todo.id} {...todo} />
+          )))}
+        </ul>
+      </div>
+    );
+  }
+};
 
 const mapStateToProps = (state) => ({
   todos: state.todos,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = { fetchTodos };
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,24 +1,11 @@
+import { getTodos } from '../lib/todoServices';
+
+const TODOS_LOAD = 'TODOS_LOAD';
 const TODO_ADD = 'TODO_ADD';
 const CURRENT_UPDATE = 'CURRENT_UPDATE';
 
 const initState = {
-  todos: [
-    {
-      id: 1,
-      name: 'Foo',
-      isComplete: false
-    },
-    {
-      id: 2,
-      name: 'Bar',
-      isComplete: true
-    },
-    {
-      id: 3,
-      name: 'Lorem',
-      isComplete: false
-    },
-  ],
+  todos: [],
   currentTodo: '',
 };
 
@@ -27,8 +14,25 @@ export const updateCurrent = (val) => ({
   payload: val,
 });
 
+export const loadTodos = (todos) => ({
+  type: TODOS_LOAD,
+  payload: todos,
+})
+
+export const fetchTodos = () => {
+  return (dispatch) => {
+    getTodos()
+      .then(todos => dispatch(loadTodos(todos)));
+  }
+}
+
 export default (state = initState, action) =>  {
   switch(action.type) {
+    case TODOS_LOAD:
+      return {
+        ...state,
+        todos: action.payload,
+      }
     case TODO_ADD:
       return {
         ...state,
